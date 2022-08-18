245acb774b2fe8306504c6efb322e69933537af6
assuntaDC <75158458+assuntaDC998@users.noreply.github.com>
2022-08-18 12:19:14 +0200

    Push lfs + mail obj addes

--- Assets/Scripts/Patrolling.cs
+++ Assets/Scripts/Patrolling.cs
@@ -26,11 +26,17 @@ namespace Enemy
             // Returns if no points have been set up
             if (points.Length == 0)
                 return;
-            // Set the agent to go to the currently selected destination.
-            agent.destination = points[destPoint].position;
-            // Choose the next point in the array as the destination,
-            // cycling to the start if necessary.
-            destPoint = (destPoint + 1) % points.Length;
+
+            if (destPoint < points.Length){
+                // Set the agent to go to the currently selected destination.
+                agent.destination = points[destPoint].position;
+                // Choose the next point in the array as the destination,
+                // cycling to the start if necessary.
+                destPoint = (destPoint + 1);
+            }
+            
+            // GameObject.Find("Enemy").active = false;
+            
         }
     }
 }
---
+++ Assets/Scripts/TimedSpawn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedSpawn : MonoBehaviour
+{
+    // public Transform enemy;
+    public GameObject spawnee;
+    public bool stopSpawning = false;
+    public float spawnDelay;
+    public float spawnTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    public void SpawnObject()
+    {
+        GameObject clone = Instantiate(spawnee, new Vector3(10.0f, 0.12f, 622.28f), transform.rotation);
+        clone.active = true;
+        if (stopSpawning)
+        {
+            CancelInvoke("SpawnObject");
+        }
+    }
+
+
+
+}
